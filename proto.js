5c65457d1d60b156952d29039ddd2778fd09ceff
KRISHNA MISHRA <98013228+krishnamishrakk@users.noreply.github.com>
2022-04-04 13:47:54 +0530

    Add files via upload

---
+++ proto.js
@@ -0,0 +1,62 @@
+// // inherited and property
+
+
+// //q1 Create 2 objects - parent and child.
+// // Attach methods to parent and use those methods 
+// //in child object using parents prototype
+
+//  let parent={
+//     cousre:"fullstack",
+//     mode:"online"
+// }
+// let res;
+// let child={
+//     name:"akarsh gupta",
+//     lives:'kanpur'
+// }
+// child.__proto__=parent;
+// console.log(child.mode);
+// let keys=Object.keys(child)
+// console.log(keys);
+// console.log(child.hasOwnProperty('mode'));
+// console.log(child.hasOwnProperty('name'));
+
+
+// //  Add a method to calculate sum of all elements
+// // in Array in array's protype, use that
+// // method to calculate sum for multiple arrays
+
+
+
+
+
+// // q4  Write a JavaScript function to retrieve 
+// //all the names of object's own and inherited properties.
+
+// let obj1={
+//     music:'listen',
+//     plays:function (){
+//         console.log("i play vollytball")
+//     }}
+   
+
+// let obj2={
+//     names:'ayush',
+//     lives:'kanpur'
+// }
+// let obj3={
+//     names:'aryan',
+//     lives:'lucknow'
+// }
+
+// obj2.__proto__=obj1
+
+// for(let key in obj2){
+//     if(obj2.hasOwnProperty(key)){
+//         console.log(key,"own property");
+//     }
+//     else{
+//         console.log(key,"inherited property");
+//     }
+// }
+//
